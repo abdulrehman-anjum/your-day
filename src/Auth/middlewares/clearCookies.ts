2486034a7d828f7667b74adb2dc5bf71d53f418c
abdulrehman-anjum <mabdulrehmananjum123@gmail.com>
2023-08-06 18:46:35 +0500

    new Change Back to the future

--- src/Auth/middlewares/clearCookies.ts
+++ src/Auth/middlewares/clearCookies.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 
 export default function(req: Request, res: Response, next: NextFunction){
-    res.clearCookie("username");
-    res.cookie("loggedOut", true);
-    next();
+    res.clearCookie("username")
+    res.cookie("loggedOut", true)
+    next()
 }
